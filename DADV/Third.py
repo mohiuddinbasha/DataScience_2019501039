1e5fde688e66d7784d8446c5b3e0df0a8ec42792
mohiuddinbasha <babuirikimani@msitprogram.net>
2021-01-02 12:55:40 +0530

    Exam

--- DADV/Third.py
+++ DADV/Third.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
 
 csvd = pd.read_csv('Daily.csv')
 
@@ -6,26 +8,34 @@ csvd.sort_values(['Gain or Loss'],inplace=True)
 
 top25d = csvd.head(25)
 print(top25d.corr())
+sn.heatmap(top25d.corr(), annot=True)
+plt.show()
 
 bottom25d = csvd.tail(25)
 print(bottom25d.corr())
-
+sn.heatmap(bottom25d.corr(), annot=True)
+plt.show()
 csvw = pd.read_csv('Weekly.csv')
 
 csvw.sort_values(['Gain or Loss'],inplace=True)
 
 top25w = csvw.head(25)
 print(top25w.corr())
-
+sn.heatmap(top25w.corr(), annot=True)
+plt.show()
 bottom25w = csvw.tail(25)
 print(bottom25w.corr())
-
+sn.heatmap(bottom25w.corr(), annot=True)
+plt.show()
 csvm = pd.read_csv('Monthly.csv')
 
 csvm.sort_values(['Gain or Loss'],inplace=True)
 
 top25m = csvm.head(25)
 print(top25m.corr())
-
+sn.heatmap(top25m.corr(), annot=True)
+plt.show()
 bottom25m = csvm.tail(25)
 print(bottom25m.corr())
+sn.heatmap(bottom25m.corr(), annot=True)
+plt.show()
